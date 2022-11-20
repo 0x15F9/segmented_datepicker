4f226eff20950d798c450ddc976b78393a215e1b
Isfaaq <isfaaq@bazaks.com>
2022-11-20 09:05:56 +0400

    create example app

--- lib/segmented_datepicker.dart
+++ lib/segmented_datepicker.dart
@@ -1,7 +1,12 @@
 library segmented_datepicker;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class SegmentedDatepicker extends StatelessWidget {
+  const SegmentedDatepicker({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('segmented');
+  }
 }
